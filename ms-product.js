b1110225c7804005cb1ec722937111b4dfcdb862
olszewskioc <thiago.olszewskioc@gmail.com>
2023-11-22 22:50:13 -0400

    Thiago Olszewski

--- ms-product.js
+++ ms-product.js
@@ -124,4 +124,63 @@ msProduct.post('/api/produtos', (request, response) => {
    });
 });
 
+msProduct.put('/api/produtos/:id', (request, response) => {
+    const productId = request.params.id;
+
+    const schema = Joi.object({
+        nome: Joi.string().max(255).required(),
+        preco: Joi.number().min(0).required(),
+        quantidade: Joi.number().integer().min(0).required()
+    });
+
+    const { value: updatedProduct, error } = schema.validate(request.body);
+
+    if (error) {
+        response.status(422);
+        response.json({
+            data: error,
+            err: true
+        });
+
+        return;
+    }
+
+    // Atualiza o produto no banco de dados
+    const stmt = db.prepare(`
+        UPDATE produtos
+        SET nome = ?, preco = ?, quantidade = ?
+        WHERE id = ?
+    `);
+
+    stmt.run([updatedProduct.nome, updatedProduct.preco, updatedProduct.quantidade, productId], (err) => {
+        if (err) {
+            response.status(500);
+            response.json({
+                data: null,
+                err: true
+            });
+            console.error(err);
+
+            return;
+        }
+
+        // Sincroniza os dados do produto alterado com o microsserviço de catálogo
+        axios.put(`http://localhost:8091/api/catalog/${productId}`, { nome: updatedProduct.nome })
+            .then(({ data: syncResponse }) => {
+                if (syncResponse.err || !syncResponse.data) {
+                    console.error('Falha ao sincronizar dados!');
+
+                    return;
+                }
+            })
+            .catch((e) => console.error(e));
+
+        response.status(200);
+        response.json({
+            data: updatedProduct,
+            err: false
+        });
+    });
+});
+
 msProduct.listen(8090, () => console.log('ms-produtos online na porta: 8090'));
